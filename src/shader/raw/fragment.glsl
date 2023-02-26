31979cf789899c2c1026179ffdbc8f3bc73cfb3b
afuscript <hfy8358@126.com>
2023-02-26 14:51:23 +0800

    chore:调整旗帜明暗度

--- src/shader/raw/fragment.glsl
+++ src/shader/raw/fragment.glsl
@@ -5,8 +5,8 @@ varying float deep; // 接收顶点着色器传递过来的deep
 uniform sampler2D uTexture; // 设置采样的纹理
 
 void main(){
-     // float deeps = deep + 0.05*10.0; // 不能直接修改deep的值，定义一个中间变量deeps接收修改的值
+     float deeps = deep + 0.05*20.0; // 不能直接修改deep的值，定义一个中间变量deeps接收修改的值
      vec4 textureColor = texture2D(uTexture,vUv);
-     // textureColor.rgb *= deeps;
+     textureColor.rgb *= deeps;
      gl_FragColor = textureColor;
 }
